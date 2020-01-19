cf3ac1c2affe376b852a23d2f0a294fbc41878b0
zhuxiaohui <www@DESKTOP-SL3U138.localdomain>
2020-01-19 09:30:25 +0800

    add sentry

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,7 +1,11 @@
 from flask import Flask
 
+from raven.contrib.flask import Sentry
+
 app = Flask(__name__,
             template_folder="../../frontend/dist",
             static_folder="../../frontend/dist/static")
+
+sentry = Sentry(app, dsn='https://f8ece1bc0e4a4a809a7f9a735c88f0a6:18db107f22f741e5b6b31e1a4063d39c@sentry.xialv.com/2')
 
 from app import routes
